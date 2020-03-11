2230bff709bae90e0f288daf7aa53f6b58e9112b
GNeto <glaudistonson@gmail.com>
2020-03-10 23:53:23 -0300

    finalized purchase process

--- index.js
+++ index.js
@@ -17,14 +17,97 @@ paypal.configure({
 });
 
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
 
     res.render("index");
 
 });
 
+app.post('/buying', (req, res) => {
+
+    var email = req.body.email;
+    var id = req.body.id;
+
+    var {name, price, amount} = req.body;
+
+    var total = price * amount;
+
+    console.log(total);
+
+    var paying = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": `http://localhost:45567/final?email=${email}&id=${id}&total=${total}`,
+            "cancel_url": "http://cancel.url"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": name,
+                    "sku": name,
+                    "price": price,
+                    "currency": "BRL",
+                    "quantity": amount
+                }]
+            },
+            "amount": {
+                "currency": "BRL",
+                "total": total
+            },
+            "description": "This is the payment description."
+        }]
+    };
+
+    paypal.payment.create(paying, (error, payment) => {
+        
+        if(error){
+            console.log(error);
+        }else{
+
+            for(var i = 0; i < payment.links.length; i++){
+                var p = payment.links[i];
+                if(p.rel === "approval_url"){
+                    res.redirect(p.href);
+                }
+            }
+        }
+    });
+});
+
+app.get("/final", (req, res) => {
+    var payerId = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+
+    var clientMail = req.query.email;
+    var clientId = req.query.id;
+    var total = req.query.total;
+
+    console.log(clientMail);
+    console.log(clientId);
+
+    var final = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount":{
+                "currency": "BRL",
+                "total": total
+            }
+        }]
+    }
+
+    paypal.payment.execute(paymentId,final,(error, payment) => {
+
+        if(error){
+            console.log(error);
+        }else{
+            res.json(payment);
+        }
+    });
+});
 
 app.listen(45567, () => {
     console.log("Running!")
 })
-
